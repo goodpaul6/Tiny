e221a90d4b89f562244c10d3684221550707a378
Apaar Madan <apaar_madan@hotmail.com>
2019-08-18 23:27:05 -0400

    Added error test to lexer

--- test/src/test_lexer.c
+++ test/src/test_lexer.c
@@ -11,7 +11,7 @@
 
 int main(int argc, char** argv)
 {
-    const char* s = "( ) { } + - * / % > < = ! & | , ; : . && || := :: += -= *= /= %= |= &= == != <= >= null true false 'a' 1 1.5 \"hello, world\" abcd_1234 if else while for return func foreign struct new cast";
+    const char* s = "( ) { } + - * / % > < = ! & | , ; : . && || := :: += -= *= /= %= |= &= == != <= >= null true false 'a' 1 1.5 \"hello, world\" abcd_1234 if else while for return func foreign struct new cast @";
 
     Tiny_Context ctx = { NULL, Tiny_DefaultAlloc };
 
@@ -92,6 +92,8 @@ int main(int argc, char** argv)
     EXPECT(TOK_STRUCT);
     EXPECT(TOK_NEW);
     EXPECT(TOK_CAST);
+
+    EXPECT(TOK_ERROR);
 
     DestroyLexer(&lexer);
 
