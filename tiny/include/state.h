b75380322d1d8fcc463fc8cfba8a9f355b08ada4
Apaar Madan <apaar_madan@hotmail.com>
2020-11-28 04:31:57 -0500

    Some more thoughts about the module system

--- tiny/include/state.h
+++ tiny/include/state.h
@@ -17,7 +17,31 @@
 // the existence of modules this way. Anytime you compile some code, we simply append
 // it to the end of the existing code and keep track of a module symbol which can
 // be used to look up symbols in the module. I suppose each module can maintain its
-// own symbol table; the parser won't own it. Imported modules will
+// own symbol table; the parser won't own it.
+
+// November 2020:
+// I'm lookin at the module ideas above with fresh eyes and wondering why we don't
+// isolate the modules at the frontend level completely? Couldn't we have a parser
+// per module, and any other modules it imports are resolved after the successful
+// parsing of every module? This way, a module that fails to parse (or even typecheck
+// in a later stage) doesn't affect ones that do (other than those that depend
+// on the module).
+//
+// As for the codegen, I think the idea of emitting into a single contiguous code
+// stream is fine.
+//
+// So the idea would be:
+// - Have a map of modules (name -> parser maybe)
+// - Given some source code, parse it completely
+// - Parse all of its module dependencies
+// -- Let the user provide the module resolution function (load relative file is sane default)
+// -- Ignore modules that have already been parsed
+// - If everything parses successfully, run the resolution phase
+// -- For inter-module dependencies, we must be able to examine symbol tables across parsers
+// -- Could store a "module" symbol which stores a reference to our Module struct
+// - If everything resolves successfully, start compile the modules
+// - If user is in "optimized" mode, we can discard all of the frontend (ASTs, symbols, etc)
+// - Run code, be happy
 
 typedef struct Tiny_State Tiny_State;
 
