7ad1c1f87e5ccd76083dd44d078ddee6b3a6b3e7
Apaar Madan <apaar_madan@hotmail.com>
2020-11-29 14:33:31 -0500

    Added basic test for resolve.c

--- test/src/test_resolve.c
+++ test/src/test_resolve.c
@@ -1,8 +1,25 @@
 #include <assert.h>
 
-#include "common.c"
-#include "context.c"
-#include "lexer.c"
-#include "map.c"
-#include "stringpool.c"
-#include "type.c"
+#include "tiny.c"
+
+int main(int argc, char** argv) {
+    Tiny_Context ctx = {NULL, Tiny_DefaultAlloc};
+
+    Parser parser;
+    Resolver resolver;
+
+    InitParser(&parser, &ctx);
+    InitResolver(&resolver, &ctx, &parser.sp, &parser.sym, &parser.tp);
+
+    const char* code = "func add(x: int, y: int): int { return x + y } add(10, 20)";
+
+    bool res = Parse(&parser, "test", code, strlen(code));
+
+    assert(res);
+
+    res = ResolveProgram(&resolver, parser.asts);
+
+    assert(res);
+
+    return 0;
+}
