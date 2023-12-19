5a17d4537f5e3dc8bea7c88f35d590dfcd82d8b0
Apaar Madan <apaar_madan@hotmail.com>
2023-12-18 23:58:55 -0500

    Add basic interpreter example

---
+++ examples/terp/src/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#include "tiny.h"
+
+int main(int argc, char** argv) {
+    if(argc < 2) {
+        fprintf(stderr, "Usage: %s (path to tiny file)\n", argv[0]);
+        return 1;
+    }
+
+    Tiny_State* state = Tiny_CreateState();
+
+    Tiny_BindStandardIO(state);
+    Tiny_BindStandardArray(state);
+    Tiny_BindStandardDict(state);
+    Tiny_BindStandardLib(state);
+
+    Tiny_CompileFile(state, argv[1]);
+
+    Tiny_StateThread stateThread;
+
+    Tiny_InitThread(&stateThread, state);
+
+    Tiny_StartThread(&stateThread);
+
+    while(Tiny_ExecuteCycle(&stateThread));
+
+    Tiny_DeleteState(state);
+
+    return 0;
+}
